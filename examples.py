f72f6784a848a04acae1205e7de6f1eb183b831c
EarthScientist <lindgren.mike@gmail.com>
2016-01-10 00:18:21 -0900

    cleaning up

--- examples.py
+++ examples.py
@@ -1,26 +1,29 @@
 # # # # 
 # Examples of how to run downscaling with this new package.
+# this is hardwired junk unless you are on the SNAP servers at UAF.
 # # # # 
 
 # AR5
 if __name__ == '__main__':
-	# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	# example of use of the new DownscaleAR5 / DownscalingUtils classes
-	# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+	# import modules
 	from downscale import DownscaleAR5
-	# input args
+	
+	# minimum required arguments
 	ar5_modeled = '/workspace/Shared/Tech_Projects/ESGF_Data_Access/project_data/data/prepped/clt_prepped/IPSL-CM5A-LR/clt/clt_Amon_IPSL-CM5A-LR_rcp26_r1i1p1_200601_210012.nc'
 	ar5_historical = '/workspace/Shared/Tech_Projects/ESGF_Data_Access/project_data/data/prepped/clt_prepped/IPSL-CM5A-LR/clt/clt_Amon_IPSL-CM5A-LR_historical_r1i1p1_185001_200512.nc'
 	clim_path = '/workspace/Shared/Tech_Projects/ALFRESCO_Inputs/project_data/TEM_Data/cru_october_final/cru_cl20/cld/akcan'
 	template_raster_fn = '/workspace/Shared/Tech_Projects/ALFRESCO_Inputs/project_data/TEM_Data/templates/tas_mean_C_AR5_GFDL-CM3_historical_01_1860.tif'
 	base_path = '/atlas_scratch/malindgren/CMIP5/TEST_AR5'
 
-	# EXAMPLE RUN -- TESTING
+	# run
 	down = DownscaleAR5.DownscaleAR5( ar5_modeled, ar5_historical, base_path, clim_path, template_raster_fn=template_raster_fn, ncores=32 ) #, climatology_begin, climatology_end, plev, absolute, metric, ncores )
 	output = down.downscale_ar5_ts()
 
 # CRU
 if __name__ == '__main__':
+
+	# import modules
 	from downscale import DownscaleCRU
 	
 	# example of post_downscale_function - pass in at DownscaleCRU()
@@ -35,7 +38,7 @@ if __name__ == '__main__':
 	template_raster_fn = '/workspace/Shared/Tech_Projects/ALFRESCO_Inputs/project_data/TEM_Data/templates/tas_mean_C_AR5_GFDL-CM3_historical_01_1860.tif'
 	base_path = '/atlas_scratch/malindgren/CMIP5/CRU2'
 	
-	# run example
+	# run
 	down = DownscaleCRU.DownscaleCRU( cru_ts, clim_path, template_raster_fn, base_path, absolute=False, ncores=32 )
 	output = down.downscale_cru_ts()
 
