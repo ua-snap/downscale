8ca484396aff164916ed1071901a94b94a59cf58
EarthScientist <lindgren.mike@gmail.com>
2016-01-14 23:13:49 -0900

    removes some GDAL testing from borrowed script

--- scripts/travis_gdal_install.sh
+++ scripts/travis_gdal_install.sh
@@ -57,15 +57,15 @@ fi
 ls -l $GDALINST
 
 # download and compile gdal version
-if [ ! -d $GDALINST/gdal-1.9.2 ]; then
-  cd $GDALBUILD
-  wget http://download.osgeo.org/gdal/gdal-1.9.2.tar.gz
-  tar -xzf gdal-1.9.2.tar.gz
-  cd gdal-1.9.2
-  ./configure --prefix=$GDALINST/gdal-1.9.2 $GDALOPTS
-  make -s -j 2
-  make install
-fi
+# if [ ! -d $GDALINST/gdal-1.9.2 ]; then
+#   cd $GDALBUILD
+#   wget http://download.osgeo.org/gdal/gdal-1.9.2.tar.gz
+#   tar -xzf gdal-1.9.2.tar.gz
+#   cd gdal-1.9.2
+#   ./configure --prefix=$GDALINST/gdal-1.9.2 $GDALOPTS
+#   make -s -j 2
+#   make install
+# fi
 
 if [ ! -d $GDALINST/gdal-1.11.2 ]; then
   cd $GDALBUILD
@@ -77,15 +77,15 @@ if [ ! -d $GDALINST/gdal-1.11.2 ]; then
   make install
 fi
 
-if [ ! -d $GDALINST/gdal-2.0.1 ]; then
-  cd $GDALBUILD
-  wget http://download.osgeo.org/gdal/2.0.1/gdal-2.0.1.tar.gz
-  tar -xzf gdal-2.0.1.tar.gz
-  cd gdal-2.0.1
-  ./configure --prefix=$GDALINST/gdal-2.0.1 $GDALOPTS
-  make -s -j 2
-  make install
-fi
+# if [ ! -d $GDALINST/gdal-2.0.1 ]; then
+#   cd $GDALBUILD
+#   wget http://download.osgeo.org/gdal/2.0.1/gdal-2.0.1.tar.gz
+#   tar -xzf gdal-2.0.1.tar.gz
+#   cd gdal-2.0.1
+#   ./configure --prefix=$GDALINST/gdal-2.0.1 $GDALOPTS
+#   make -s -j 2
+#   make install
+# fi
 
 # change back to travis build dir
 cd $TRAVIS_BUILD_DIR
