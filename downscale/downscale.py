e2e2fed3d5c80581047868a5c20c47116056a1e9
EarthScientist <lindgren.mike@gmail.com>
2016-02-22 10:19:57 -0900

    adds new downscaling classes and modifications to the snap_scripts.

--- downscale/downscale.py
+++ downscale/downscale.py
@@ -7,6 +7,7 @@
 # # #
 import rasterio, os
 import numpy as np
+import pandas as pd
 from downscale import DownscalingUtils as utils #utils
 
 class Baseline( object ):
@@ -28,13 +29,17 @@ class Baseline( object ):
 		self.arrlist = [ rasterio.open( fn ).read( 1 ) for fn in self.filelist ]
 
 class Dataset( object ):
-	def __init__( self, fn, variable, model, scenario, units=None, *args, **kwargs ):
+	def __init__( self, fn, variable, model, scenario, units=None, interp=False, ncpus=2, \
+					method='cubic', *args, **kwargs ):
 		'''
 		fn = [str] path to the xray supported dataset to be read in.
 		variable = [str] abbreviation of variable name to extract from file
 		model = [str] name of the model being read
 		scenario = [str] name of the scenario being read
 		units = [str] abbreviation of the units of the variable
+		interp = [bool] if True interpolate across NA's using a spline. 
+					if False (default) do nothing.
+		ncpus = [ int ] number of cores to use if interp=True. default:2.
 		'''
 		import xarray as xr
 		self.fn = fn
@@ -43,11 +48,69 @@ class Dataset( object ):
 		self.model = model
 		self.scenario = scenario
 		self.units = units
+		self.interp = interp
+		self.ncpus = ncpus
+		if interp:
+			print( 'running interpolation across NAs' )
+			self._interpna( method=method )
 
-class AR5( object ):
+	@staticmethod
+	def rotate( dat, lons, to_pacific=False ):
+		'''rotate longitudes in WGS84 Global Extent'''
+		if to_pacific == False:
+			# to -180.0 - 180.0 
+			dat, lons = utils.shiftgrid( 180., dat, lons, start=False )
+		elif to_pacific == True:
+			# to 0 - 360
+			dat, lons = utils.shiftgrid( 0., dat, lons )
+		else:
+			raise AttributeError( 'to_pacific can be only one of True or False' )
+		return dat, lons
+	def _interpna( self, method='cubic' ):
+		'''
+		np.float32
+		method = [str] one of 'cubic', 'near', 'linear'
+		'''
+		from pathos import multiprocessing
+		# import multiprocessing
+		# output_dtype = np.float32
+		
+		rotated = None
+		# [GOTCHA]: if greenwich-centered, lets rotate it to pcll since we are interested in ALASKA
+		if not ( self.ds.lon > 200.0 ).any() == True:
+			dat, lons = self.rotate( self.ds[ self.variable ].data, self.ds.lon, to_pacific=True )
+			rotated = True
+		else:
+			dat, lons = self.ds[ self.variable ].data, self.ds.lon
+
+		# mesh the lons and lats and unravel them to 1-D
+		xi,yi = np.meshgrid( lons, self.ds.lat )
+		lo, la = [ i.ravel() for i in (xi,yi) ]
+
+		# setup args for multiprocessing
+		args = [ pd.DataFrame( {'x':lo,'y':la,'z':d.ravel()} ).dropna( axis=0, how='any' ).to_dict( orient='list' ) for d in dat ]
+		[ arg.update( grid=(xi,yi), method=method ) for arg in args ] #, output_dtype=output_dtype
+		def wrap( x ):
+			return utils.xyz_to_grid( **x )
+			# return xyz_to_grid( **x )
+		print self.ncpus
+		pool = multiprocessing.Pool( self.ncpus )
+		out = pool.map( wrap, args )
+		pool.close()
+
+		# stack em and roll-its axis so time is dim0
+		dat = np.rollaxis( np.dstack( out ), -1 )
+		if rotated: # rotate it back
+			dat, lons = self.rotate( dat, lons, to_pacific=True )
+		# place back into a new xarray.Dataset object for further processing
+		self.ds[ self.variable ].data = dat
+		print( 'ds interpolated and placed back into self.ds' )
+
+
+class DeltaDownscale( object ):
 	def __init__( self, baseline, clim_begin, clim_end, historical, future=None, \
 		metric='mean', ds_type='absolute', level=None, level_name=None ):
-		'''	
+		'''
 		this class appears to be working correctly for the CRU data as well
 		this is great and means it should be built as a generic downscale class
 		# a class for the AR5 would need to have
@@ -112,21 +175,33 @@ class AR5( object ):
 		self.anomalies = anomalies
 	@staticmethod
 	def interp_ds( anom, base, output_filename, src_transform, downscaling_operation, post_downscale_function=None, mask=None, mask_value=0 ):
+		'''	
+		anom = [numpy.ndarray] 2-d array representing a single monthly timestep of the data to be downscaled. Must also be representative of anomalies.
+		base = [str] filename of the corresponding baseline monthly file to use as template and downscale baseline for combining with anomalies.
+		output_filename = [str] path to the output file to be created following downscaling
+		src_transform = [affine.affine] 6 element affine transform of the input anomalies. [should be greenwich-centered]
+		downscaling_operation = [str] one of 'add' or 'mult' depending on absolute or relative delta downscaling.
+		post_downscale_function = [function] function that takes as input a single 2-d array and returns a 2-d array in the same shape as the input.  
+		mask = [numpy.ndarray] 2-d array showing what values should be masked. Masked=0, unmasked=1. must be same shape as base.
+		mask_value = [int] what value to use as the masked values. default=0.
+
+		'''		
 		from rasterio.warp import reproject, RESAMPLING
 		# reproject / resample
 		src_crs = {'init':'epsg:4326'}
 		src_nodata = None # DangerTown™
+		base = rasterio.open( base )
+		baseline_arr = base.read( 1 )
 		baseline_meta = base.meta
 		baseline_meta.update( compress='lzw' )
-		baseline_arr = base.read( 1 )
-		output_arr = np.empty_like( base.read( 1 ) ) # base should be a rasterio obj not arr... check this.
+		output_arr = np.empty_like( baseline_arr )
 
 		# TODO: make this function available for manipulation if used for different needs
-		reproject( output_arr, baseline_arr, src_transform=src_transform, src_crs=src_crs, src_nodata=src_nodata, \
+		reproject( anom, output_arr, src_transform=src_transform, src_crs=src_crs, src_nodata=src_nodata, \
 				dst_transform=baseline_meta['affine'], dst_crs=baseline_meta['crs'],\
 				dst_nodata=None, resampling=RESAMPLING.cubic_spline, SOURCE_EXTRA=1000 )
 		# downscale
-		return utils.downscale( anom, output_arr, output_filename, downscaling_operation, \
+		return utils.downscale( output_arr, baseline_arr, output_filename, downscaling_operation, \
 				baseline_meta, post_downscale_function, mask, mask_value )
 	def downscale( self, output_dir, prefix=None, ncpus=32 ):
 		import affine
@@ -134,6 +209,7 @@ class AR5( object ):
 		from functools import partial
 		# import multiprocessing
 		from pathos import multiprocessing
+		
 		# output_filenames 
 		time = self.anomalies.time.to_pandas()
 		time_suffix = [ '_'.join([str(t.month), str(t.year)]) for t in time ]
@@ -156,82 +232,30 @@ class AR5( object ):
 			dat, lons = ( self.anomalies, self.anomalies.lon )
 			src_transform = self.affine
 		# run and output
-		rstlist = [ rasterio.open( fn ) for fn in self.baseline.filelist ]
-		# # # # # 
-		# maybe itertools.repeat is needed here? # these are same references
-		rstlist = itertools.repeat( rstlist, (self.anomalies.shape[0] / 12) )
-		rstlist = [ j for i in rstlist for j in i ]
-		# rstlist = rstlist * (self.anomalies.shape[0] / 12)
-		# # # # # 
+		rstlist = self.baseline.filelist * (self.anomalies.shape[0] / 12)
 		args = zip( self.anomalies, rstlist, output_filenames )
 		args = [{'anom':i.data, 'base':j, 'output_filename':k} for i,j,k in args ]
 
+		# determine operation type
 		downscaling_operation_switch = {'absolute':'add', 'relative':'mult'}
 		downscaling_operation = downscaling_operation_switch[ self.ds_type ]
-		
-		# anom_arr, baseline_arr, output_filename,	downscaling_operation, \
-		# 	meta, post_downscale_function, mask=None, mask_value=0
+		# partial and wrapper
+		f = partial( self.interp_ds, src_transform=src_transform, downscaling_operation=downscaling_operation, \
+						post_downscale_function=None, mask=None, mask_value=0 )
+		def wrap( d ):
+			return f( **d )
 
-		f = partial( self.interp_ds, src_transform=src_transform, downscaling_operation=downscaling_operation, post_downscale_function=None, mask=None, mask_value=0 )
 		pool = multiprocessing.Pool( ncpus )
-		out = pool.map( lambda x: f( **x ), args[:11] )
-		pool.join()
+		# out = pool.map( lambda x: f( **x ), args )
+		out = pool.map( wrap, args )
+		# pool.join()
 		pool.close()
 		return output_dir
-
-
 
 #  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 
 
-
-
-def downscale_wrapper( arr, affine, crs, baseline, output_filename, downscaling_operation, post_downscale_function ):
-	# rotate
-	if ( self.data.ds.lon > 200.0 ).any() == True:
-		dat, lons = utils.shiftgrid( 180., self.data.anomalies, self.historical.ds.lon, start=False )
-		a,b,c,d,e,f,g,h,i = affine #flip it to the greenwich-centering
-		src_transform = affine.Affine( a, b, -180.0, d, e, 180.0 )
-	else:
-		dat, lons = ( self.data.ds, self.historical.ds.lon )
-		src_transform = affine
-	
-	# reproject / resample
-	src_crs = {'init':'epsg:4326'}
-	src_nodata = None # DangerTown™
-	baseline_meta = baseline.meta
-	baseline_meta.update( compress='lzw' )
-	output_arr = np.empty_like( baeline.read( 1 ) )
-
-	# TODO: make this function available for manipulation if used for different needs
-	reproject( arr, output_arr, src_transform=src_transform, src_crs=src_crs, src_nodata=src_nodata, \
-			dst_transform=baseline_meta['affine'], dst_crs=baseline_meta['crs'],\
-			dst_nodata=None, resampling=RESAMPLING.cubic_spline, SOURCE_EXTRA=1000 )
-	
-	# downscale
-	return utils.downscale( arr, output_arr, output_filename, downscaling_operation, \
-					baseline_meta, post_downscale_function, mask=None, mask_value=0 )
-
-
-def run_downscale( data, baseline, output_path, postprep_function, ncpus=2 ):
-	from functools import partial
-	output_filenames = [ os.path.join( output_path, monyear + '.tif' ) for monyear in monyear ]
-	data_base_fn = zip( data.ds, baseline.arr_list, output_filenames )
-	pool = multiprocessing.Pool( ncpus )
-	f = partial( downscale_wrapper,  )
-	out = pool.map( f, data_base_fn )
-
-
-
-
-# def downscale( data, baseline, output_filenames, postprep_function, ncpus=2 ):
-# 	from downscale import utils
-# 	import multiprocessing
-
-
-
-
-
+# OLDER LEGACY CLASS -- NON-WORKING:
 # class Downscale( object ):
 # 	''' downscaling... '''
 # 	def __init__( self, data, baseline, *args, **kwargs ):
---
+++ snap_scripts/downscale_snap.py
@@ -0,0 +1,30 @@
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+# BASELINE:
+import glob, os
+clim_path = '/workspace/Shared/Tech_Projects/ALFRESCO_Inputs/project_data/TEM_Data/cru_october_final/cru_cl20/cld/akcan'
+filelist = glob.glob( os.path.join( clim_path, '*.tif' ) )
+baseline = Baseline( filelist )
+
+# DATASET:
+fn = '/workspace/Shared/Tech_Projects/ESGF_Data_Access/project_data/snap_prepped_data/IPSL-CM5A-LR/hur/hur_Amon_IPSL-CM5A-LR_rcp26_r1i1p1_200601_210012.nc'
+variable = 'hur'
+model = 'IPSL-CM5A-LR'
+scenario = 'rcp26'
+
+# ar5 modeled
+ds = Dataset( fn, variable, model, scenario, units=None )
+clim_begin = '2061'
+clim_end = '2090'
+ar5 = DeltaDownscale( baseline, clim_begin, clim_end, ds, future=None, \
+		metric='mean', ds_type='absolute', level=1000, level_name='plev' )
+ar5.downscale( output_dir='/workspace/Shared/Tech_Projects/ESGF_Data_Access/project_data/test' )
+
+# CRU historical
+cru_ts = '/Data/Base_Data/Climate/World/CRU_grids/CRU_TS323/cru_ts3.23.1901.2014.cld.dat.nc'
+cru_ds = Dataset( cru_ts, 'cld', 'cru_ts31', 'observed', units=None )
+clim_begin = '1961'
+clim_end = '1990'
+cru = DeltaDownscale( baseline, clim_begin, clim_end, cru_ds, metric='mean', ds_type='relative' )
+cru.downscale( output_dir='/workspace/Shared/Tech_Projects/ESGF_Data_Access/project_data/test' )
+
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
