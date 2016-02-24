928f6d34b64989fc27ae31feb304dcf2fa80764a
EarthScientist <lindgren.mike@gmail.com>
2016-02-23 21:26:54 -0900

    update travis-ci yml and test_utils.py for CI build

--- downscale/tests/test_utils.py
+++ downscale/tests/test_utils.py
@@ -5,15 +5,13 @@
 
 import unittest
 import rasterio
+import numpy as np
 
 class TestWriteGTiff( unittest.TestCase ):
 	''' tests for testing write_gtiff function '''
 	def setUp( self ):
 		print( 'setup' )
-		import rasterio
 		from affine import Affine
-		import numpy as np
-
 		# make dummy rasters / arrays for testing
 		meta = {'affine': Affine(1.0, 0.0, -180.0, 0.0, -1.0, 180.0),
 				'count': 1,
@@ -27,20 +25,20 @@ class TestWriteGTiff( unittest.TestCase ):
 		with rasterio.drivers():
 			self.rst = rasterio.open( '', mode='w', **meta )
 			self.arr = self.rst.read( 1 )
-			meta.update( driver=u'GTiff')
+			meta.update( driver=u'GTiff' )
 			self.meta = meta
 			self.test_fn = './test_write_gtiff_59137.tif'
 	def test_output_crs( self ):
 		print( 'test_meta' )
 		import rasterio
-		from downscale.DownscalingUtils import write_gtiff
+		from downscale.utils import write_gtiff
 		out = write_gtiff( self.arr, self.meta, self.test_fn )
 		new_meta = rasterio.open( out ).meta
 		self.assertEqual( self.meta['crs'], new_meta['crs'] )
 	def test_output_shape( self ):
 		print( 'test_shape' )
 		import rasterio
-		from downscale.DownscalingUtils import write_gtiff
+		from downscale.utils import write_gtiff
 		out = write_gtiff( self.arr, self.meta, self.test_fn )
 		new_meta = rasterio.open( out ).meta
 		self.assertEqual( self.meta['height'], new_meta['height'] )
