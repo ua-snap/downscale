4786c0d996fda0c9603ed60bd09a8b12f25cc729
EarthScientist <lindgren.mike@gmail.com>
2016-09-02 15:08:36 -0800

    more masking attempts

--- snap_scripts/tem_inputs_iem/cru_cl20_preprocess.py
+++ snap_scripts/tem_inputs_iem/cru_cl20_preprocess.py
@@ -147,7 +147,7 @@ if __name__ == '__main__':
 		with rasterio.open( final_fn, 'w', **template_meta ) as out:
 			out.write( mask, 1 )
 
-		os.system( 'gdalwarp -wo SOURCE_EXTRA=100 -multi -src_nodata -3.4e+38 -dstnodata -3.4e+38 {} {}'.format( fn.replace( 'PCLL', 'LL' ), final_fn ) )
+		os.system( 'gdalwarp -wo SOURCE_EXTRA=100 -multi -srcnodata -3.4e+38 -dstnodata -3.4e+38 {} {}'.format( fn.replace( 'PCLL', 'LL' ), final_fn ) )
 		# os.system( 'gdalwarp -overwrite -t_srs EPSG:3338 -co COMPRESS=LZW -wo SOURCE_EXTRA=100 -multi -srcnodata {} -dstnodata {} {} {}'.format( -3.4e+38, -3.4e+38, fn.replace( 'PCLL', 'LL' ), final_fn ) )
 		new_paths = new_paths + [ final_fn ]
 	print( 'completed run of {}'.format( variable ) )
