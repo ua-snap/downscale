0e23b5bdb976c1699a011619e0d5db585acef8f6
EarthScientist <lindgren.mike@gmail.com>
2016-09-03 14:46:52 -0800

    adds a call to `.to_dataset()` to have xarray re-sort itself into a dataset obj from the concatenated files.  This hack appears to get around the loss of date issue.

--- downscale/preprocess.py
+++ downscale/preprocess.py
@@ -101,14 +101,16 @@ class Preprocess( object ):
 			# THIS should be used, but it is being a PITA
 			try:
 				ds = xarray.concat([ xarray.open_dataset( i ).load() for i in self.filelist ], 'time' )
-				ds = ds.sel( time=slice( str(self.years[0]), str(self.years[1]) ) )
+				ds = ds[self.variable].sel( time=slice( str(self.years[0]), str(self.years[1]) ) )
+				ds = ds.to_dataset() # new
 			except RuntimeWarning:	
 				ds = reduce( lambda x,y: xarray.concat( [x,y], 'time'), (xarray.open_dataset( i ) for i in self.filelist) )
 
 				ds = self._year_greater_yearlimit_workaround( ds, self.years[0], self.years[1], int(self._fileyears_dict['minyear']), int(self._fileyears_dict['maxyear']) )
 				if ds.time.dtype =='O':
 					print( '\nWARNING: Preprocess: modified time -- bad type\n' )
-					ds['time'] = pd.date_range(str(self.years[0]), str(self.years[1]+1), freq='M') # MONTHLY so leap doesnt matter and neither do days
+					ds.loc['time'] = pd.date_range(str(self.years[0]), str(self.years[1]+1), freq='M') # MONTHLY so leap doesnt matter and neither do days
+				ds = ds[self.variable].to_dataset()
 		return ds
 	def write_nc( self, output_path=None, overwrite=True, nc_format='NETCDF4' ):
 		'''
