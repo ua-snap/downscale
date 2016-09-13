a71b3a682d851f962f346ad49e5a47d1babb3a51
EarthScientist <lindgren.mike@gmail.com>
2016-09-12 19:39:56 -0800

    adds filename sorter to baseline rstlist

--- downscale/ds_minmax.py
+++ downscale/ds_minmax.py
@@ -15,6 +15,54 @@ import os
 import numpy as np
 import xarray as xr
 
+def sort_files( files, split_on='_', elem_month=-2, elem_year=-1 ):
+	'''
+	sort a list of files properly using the month and year parsed
+	from the filename.  This is useful with SNAP data since the standard
+	is to name files like '<prefix>_MM_YYYY.tif'.  If sorted using base
+	Pythons sort/sorted functions, things will be sorted by the first char
+	of the month, which makes thing go 1, 11, ... which sucks for timeseries
+	this sorts it properly following SNAP standards as the default settings.
+	ARGUMENTS:
+	----------
+	files = [list] list of `str` pathnames to be sorted by month and year. usually from glob.glob.
+	split_on = [str] `str` character to split the filename on.  default:'_', SNAP standard.
+	elem_month = [int] slice element from resultant split filename list.  Follows Python slicing syntax.
+		default:-2. For SNAP standard.
+	elem_year = [int] slice element from resultant split filename list.  Follows Python slicing syntax.
+		default:-1. For SNAP standard.
+	RETURNS:
+	--------
+	sorted `list` by month and year ascending. 
+	'''
+	import pandas as pd
+	months = [ int(fn.split('.')[0].split( split_on )[elem_month]) for fn in files ]
+	years = [ int(fn.split('.')[0].split( split_on )[elem_year]) for fn in files ]
+	df = pd.DataFrame( {'fn':files, 'month':months, 'year':years} )
+	df_sorted = df.sort_values( ['year', 'month' ] )
+	return df_sorted.fn.tolist()
+
+def only_years( files, begin=1901, end=2100, split_on='_', elem_year=-1 ):
+	'''
+	return new list of filenames where they are truncated to begin:end
+	ARGUMENTS:
+	----------
+	files = [list] list of `str` pathnames to be sorted by month and year. usually from glob.glob.
+	begin = [int] four digit integer year of the begin time default:1901
+	end = [int] four digit integer year of the end time default:2100
+	split_on = [str] `str` character to split the filename on.  default:'_', SNAP standard.
+	elem_year = [int] slice element from resultant split filename list.  Follows Python slicing syntax.
+		default:-1. For SNAP standard.
+	RETURNS:
+	--------
+	sliced `list` to begin and end year.
+	'''
+	import pandas as pd
+	years = [ int(fn.split('.')[0].split( split_on )[elem_year]) for fn in files ]
+	df = pd.DataFrame( { 'fn':files, 'year':years } )
+	df_slice = df[ (df.year >= begin ) & (df.year <= end ) ]
+	return df_slice.fn.tolist()
+
 def delta_mm( fn, mean_fn, variable, mean_variable='tas' ):
 	'''
 	simple way to compute extreme - mean deltas as 
@@ -106,7 +154,8 @@ class DeltaDownscaleMinMax( DeltaDownscale ):
 			print( 'anomalies NOT rotated!' )
 
 		# run and output
-		rstlist = self.baseline.filelist
+		# rstlist = self.baseline.filelist
+		rstlist = only_years( sort_files( self.baseline.filelist ), begin=self.historical.begin, end=self.historical.end )
 		
 		if isinstance( self.anomalies_rot, xr.Dataset ):
 			self.anomalies_rot = self.anomalies_rot[ self.historical.variable ].data
