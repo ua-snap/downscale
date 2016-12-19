19da1d17ca067e1a5ea2fe7b3f4e6909d0d5c7bd
EarthScientist <lindgren.mike@gmail.com>
2016-12-19 11:15:23 -0900

    adds scripts to compute 5ModelAvg and crop clip to the ALF AOI.

---
+++ snap_scripts/tem_inputs_v2/crop_clip_to_iem_aoi.py
@@ -0,0 +1,44 @@
+def crop_clip( shp_fn, rst_fn, out_fn ):
+	'''
+	crop/clip to the shapefile we want using gdalwarp.
+	'''
+	import subprocess, os
+	try:
+		dirname = os.path.dirname( out_fn )
+		if not os.path.exists( dirname ):
+			os.makedirs( dirname )
+	except:
+		pass
+
+	# proj4string = '+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
+	proj4string = 'EPSG:3338'
+	subprocess.call(['gdalwarp', '-q', '-tap','-overwrite', '-tap' ,'-t_srs', proj4string,'-co', 'COMPRESS=LZW', '-tr', '2000', '2000', 
+						'-srcnodata', '-9999', '-dstnodata', '-9999', '-crop_to_cutline', '-cutline', 
+						shp_fn, rst_fn, out_fn ])
+	return out_fn
+
+def wrap( x ):
+	''' wrapper for clean multiprocessing call to pool.map '''
+	return crop_clip( *x )
+
+if __name__ == '__main__':
+	import os, glob, itertools, rasterio
+	import xarray as xr
+	import numpy as np
+	import pandas as pd
+	from pathos.mp_map import mp_map
+
+	# setup args
+	base_path = '/workspace/Shared/Tech_Projects/ESGF_Data_Access/project_data/tem_data_sep2016/downscaled'
+	output_path = '/workspace/Shared/Tech_Projects/ESGF_Data_Access/project_data/tem_data_sep2016/downscaled_iem'
+	ncpus = 32
+	subdomain_fn = '/workspace/Shared/Tech_Projects/Alaska_IEM/project_data/ALFRESCO/aiem_domain_fixed/AIEM_domain.shp'
+ 
+	# list up all the args we want to run through the multicore clipping
+	args_list = []
+	for root, subs, files in os.walk( base_path ):
+		tif_files = [ fn for fn in files if fn.endswith( '.tif' ) ]
+		if len( tif_files ) > 0:
+			args_list = args_list + [ ( subdomain_fn, os.path.join( root, fn ), os.path.join( root, fn ).replace( base_path, output_path ) ) for fn in tif_files ]
+	
+	out = mp_map( wrap, args_list, nproc=ncpus )
---
+++ snap_scripts/tem_inputs_v2/resample_to_1km.py
@@ -0,0 +1,33 @@
+# resample to 1km
+def run( fn, out_fn ):
+	import os
+	dirname = os.path.dirname( out_fn )
+	try:
+		if not os.path.exists( dirname ):
+			os.makedirs( dirname )
+	except:
+		pass
+
+	os.system( 'gdalwarp -q -r bilinear -overwrite -t_srs EPSG:3338 -tr 1000 1000 -dstnodata -9999 -co COMPRESS=LZW {} {}'.format( fn, out_fn ) )
+	return out_fn
+
+def wrap( x ):
+	return run( *x )
+
+if __name__ == '__main__':
+	import os
+	from pathos.mp_map import mp_map
+
+	# directories
+	# base_path = '/workspace/Shared/Tech_Projects/ESGF_Data_Access/project_data/tem_data_sep2016/downscaled'
+	base_path = '/workspace/Shared/Tech_Projects/EPSCoR_Southcentral/project_data/downscaled'
+	output_path = '/workspace/Shared/Tech_Projects/ESGF_Data_Access/project_data/tem_data_sep2016/downscaled_1km'
+
+	# list the data
+	# files = [os.path.join(root, fn) for root, subs, files in os.walk( base_path ) for fn in files if fn.endswith( '.tif' )]
+	# files = [os.path.join(root, fn) for root, subs, files in os.walk( base_path ) for fn in files if fn.endswith( '.tif' ) and 'tas_' in fn or 'pr_' in fn ]
+	files = [os.path.join(root, fn) for root, subs, files in os.walk( base_path ) for fn in files if fn.endswith( '.tif' ) and 'vap_' in fn or 'rsds_' in fn ]
+	out_files = [ fn.replace( base_path, output_path ) for fn in files ]
+
+	# run it in parallel
+	done = mp_map( wrap, zip( files, out_files ), nproc=32 )
