0944f001156f22e4a63c1c92a407ba501f965350
EarthScientist <lindgren.mike@gmail.com>
2017-03-09 21:26:56 -0900

    move the interpolated and reprojected deltas to a common directory.

---
+++ snap_scripts/epscor_sc/move_anomalies_to_common_dir.py
@@ -0,0 +1,34 @@
+# move the anomalies (deltas) that have been resampled to 2km
+# to a new directory
+def move( fn, new_fn, *args ):
+	import shutil, os
+
+	dirname, basename = os.path.split( new_fn )
+	if not os.path.exists( dirname ):
+		os.makedirs( dirname )
+
+	return shutil.move( fn, new_fn )
+
+def make_args( fn ):
+	dirname, basename = os.path.split( fn )
+	new_dirname = dirname.replace( '/anom', '' ).replace( 'downscaled', 'anomalies' )
+	new_fn = os.path.join( new_dirname, basename )
+	return fn, new_fn
+
+if __name__ == '__main__':
+	import os
+	import numpy as np
+	from pathos.mp_map import mp_map
+
+	# base path 
+	base_path = '/workspace/Shared/Tech_Projects/EPSCoR_Southcentral/project_data/downscaled'
+
+	# list the dirs we want to move
+	args = [ make_args( os.path.join( root, fn ) ) for root,s,files in os.walk( base_path ) if 'anom' in root for fn in files ]
+
+	# move 'em
+	done = mp_map( lambda x: move( *x ), args, nproc=32 )
+
+	# remove the old 'anom' dirs
+	dirs = np.unique( np.array([ root for root,s,files in os.walk( base_path ) if 'anom' in root ]) ).tolist()
+	_ = [ os.system( 'rm -r {}'.format( d ) ) for d in dirs ]
