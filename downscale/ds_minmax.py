6d2d155e56ea6b4cca9c8fd6d2cb03368b4ba780
EarthScientist <lindgren.mike@gmail.com>
2017-05-22 17:05:07 -0800

    fix interp issue

--- downscale/ds_minmax.py
+++ downscale/ds_minmax.py
@@ -35,7 +35,7 @@ class DeltaDownscaleMinMax( DeltaDownscale ):
 		# setup new args
 		self.mean_ds = mean_ds
 		self.mean_variable = mean_variable
-		print *args, **kwargs
+		print kwargs
 
 		super( DeltaDownscaleMinMax, self ).__init__( *args, **kwargs )
 		
