c3abd05b6d6bffd63f77dead5c9dab99a6b501d8
EarthScientist <lindgren.mike@gmail.com>
2018-03-27 14:42:15 -0800

    updates for common mask between all climatologies at the same extent.

--- snap_scripts/baseline_climatologies/cru_cl20_climatology_preprocess_2km_RUN_EXAMPLE.py
+++ snap_scripts/baseline_climatologies/cru_cl20_climatology_preprocess_2km_RUN_EXAMPLE.py
@@ -9,7 +9,7 @@ script_path = '/workspace/UA/malindgren/repos/downscale/snap_scripts/baseline_cl
 os.chdir( script_path )
 base_path = '/workspace/Shared/Tech_Projects/DeltaDownscaling/project_data'
 cru_filenames = glob.glob('/Data/Base_Data/Climate/World/CRU_grids/CRU_TS20/*.dat.gz')
-template_raster_fn = '/workspace/Shared/Tech_Projects/DeltaDownscaling/project_data/templates/akcan_2km/tas_mean_C_AR5_CCSM4_rcp26_01_2006.tif'
+template_raster_fn = '/workspace/Shared/Tech_Projects/DeltaDownscaling/project_data/templates/akcan_2km/tas_mean_C_ar5_IPSL-CM5A-LR_rcp26_01_2006.tif'
 for cru_filename in cru_filenames:
 	print( 'working on: {}'.format( os.path.basename( cru_filename ) ) )
 	variable = os.path.basename( cru_filename ).split( '_' )[-1].split('.')[0]
