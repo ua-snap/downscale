ba0e180bea0d698f9af6ade351577fe9328d1e86
EarthScientist <lindgren.mike@gmail.com>
2018-03-28 12:55:30 -0800

    updates...

--- snap_scripts/downscaling_v2/wrap_downscaler_cru_ts40_slurm.py
+++ snap_scripts/downscaling_v2/wrap_downscaler_cru_ts40_slurm.py
@@ -23,8 +23,8 @@ if __name__ == '__main__':
 	ncores = '32'
 	model = 'ts40'
 	scenario = 'historical'
-	variables = ['pre'] #['tmp', 'pre']
-	out_varnames = ['pr'] #['tas', 'pr']
+	variables = ['tmp', 'pre']
+	out_varnames = ['tas', 'pr']
 	
 	slurm_path = os.path.join( base_dir, 'downscaled','slurm_log' )
 	if not os.path.exists( slurm_path ):
