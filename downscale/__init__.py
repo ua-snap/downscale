ed7e89dc2789266ee14f6d135673ed8c489992e3
EarthScientist <lindgren.mike@gmail.com>
2018-04-01 18:45:07 -0800

    [FF-TEST] debugging run attempt.

--- downscale/__init__.py
+++ downscale/__init__.py
@@ -1,5 +1,5 @@
 from downscale.ds import *
 from downscale.dataset import *
 from downscale.ds_minmax import *
-from downscale.ds_ff import *
-from downscale.dataset_ff import *
+# from downscale.ds_ff import *
+# from downscale.dataset_ff import *
